4a93dec006fd2ac5a8b7aa062e0ff3313180d54c
AdrianDanlos <danlosadrian@gmail.com>
2020-12-03 10:44:26 +0100

    cypress testnames rule

---
+++ tests/lib/rules/cypress_testnames.js
@@ -0,0 +1,62 @@
+/**
+ * @fileoverview Checks that cypress test names follow the 'should' naming convention
+ * @author danlosadrian
+ */
+
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/elements-have-ids"),
+
+RuleTester = require("eslint").RuleTester;
+
+RuleTester.setDefaultConfig({
+    parserOptions: {
+      ecmaVersion: 6,
+      ecmaFeatures: {
+        jsx: true,
+      },
+    }
+  });
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("elements-have-ids", rule, {
+
+    valid: [
+        {
+            code: '<button id="button1"></button>',
+        },
+        {
+            code: '<button id="button1" />',
+        },
+        {
+            code: '<input id="button1"></input>',
+        },
+        {
+            code: '<div></div>',
+        }
+      ],
+
+    invalid: [
+        {
+            code: '<button></button>',
+            errors: [{
+                message: "Common form elements should have an ID",
+                type: "JSXOpeningElement"
+            }]
+        },
+        {
+            code: '<button id=""></button>',
+            errors: [{
+                message: "Common form elements should not have empty IDs",
+                type: "JSXOpeningElement"
+            }]
+        }
+    ]
+});
